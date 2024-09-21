b3894dc4efb695b06b17e58b133c101ae972bc13
dhruv465 <dhruv.465@gmail.com>
2024-09-21 13:37:07 +0530

    Adjusting the layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -104,9 +104,8 @@ export default function Home() {
 
       <Header isMobile={isMobile} />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         <HeroSection />
-       
       </div>
       <AboutSection />
       <ProjectsSection />
