aca06b95f3e83789dfbfa692cd7fd8550a1cbbd3
dhruv465 <dhruv.sathe11@gmail.com>
2024-10-26 12:33:08 +0530

    Refactor project configuration files and add utility function
    - Refactor jsconfig.json to add compiler options for path aliases
    - Add utils.js file with cn utility function for class merging
    - Update vite.config.js to resolve aliases for easier imports
    - Add components.json file with aliases for project components
    - Update package.json dependencies and description
    - Add globals.css file with tailwind base styles
    - Refactor HeroSection.jsx to use BackgroundBeamsWithCollision component
    - Update tailwind.config.js with customizations and animations

--- Front-End/src/MyComponents/Hero/HeroSection.jsx
+++ Front-End/src/MyComponents/Hero/HeroSection.jsx
@@ -2,26 +2,35 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import Robo from '../../assets/Robo.png'
 import myImage from '../../assets/Dhruv.png'
+ import { BackgroundBeamsWithCollision } from "../../components/ui/background-beams-with-collision";
 
 export default function HeroSection() {
   return (
     <>
-      <motion.div id="home"
-        className="bg-indigo-100 p-8 rounded-3xl col-span-1 md:col-span-2 row-span-2 lg:mt-12 lg:mb-12"
+    <BackgroundBeamsWithCollision className="bg-indigo-100 p-8 rounded-3xl col-span-1 md:col-span-2 row-span-2 lg:mt-12 lg:mb-12 relative z-10">
+      <motion.div
+        id="home"
+        className="" // Ensures content is above the beams
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
         <div className="mb-4">
-          <img src="https://res.cloudinary.com/dvfrcaw1c/image/upload/v1727845117/dtpebkaltqmgdwgarjie.png" alt="Robo" className="w-16 h-16" />
+          <img
+            src="https://res.cloudinary.com/dvfrcaw1c/image/upload/v1727845117/dtpebkaltqmgdwgarjie.png"
+            alt="Robo"
+            className="w-16 h-16"
+          />
         </div>
         <h2 className="text-4xl md:text-6xl font-extrabold leading-tight title-card">
-          Dev Revolutionizing<br />
-          <span className='Architecture'> Technology </span> with<br />
+          Dev Revolutionizing
+          <br />
+          <span className="Architecture"> Technology </span> with
+          <br />
           AI-Enhanced Solutions
         </h2>
       </motion.div>
-
+    </BackgroundBeamsWithCollision>
       <motion.div
         className="bg-indig-100 rounded-3xl overflow-hidden row-span-2"
         initial={{ opacity: 0, y: 50 }}
